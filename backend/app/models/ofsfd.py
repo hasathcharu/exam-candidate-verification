39c6bb488573c9c7803dfbcba2f50f7bc5fe20b9
sagini18 <saginisaju@gmail.com>
2025-07-16 21:23:10 +0530

    bug fix

--- backend/app/models/ofsfd.py
+++ backend/app/models/ofsfd.py
@@ -1,21 +1,20 @@
 import torch.nn as nn
 import torch
 import gdown
+import os
 import clip
-from ..helpers.ofsfd.utils.constants import DEVICE, MODEL_PATH_ID
+from ..helpers.ofsfd.utils.constants import DEVICE
 import matplotlib.pyplot as plt
 from ..helpers.ofsfd.preprocess.preprocess import preprocess
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "best_model", "ofsfd.pth")
 
 class SignatureVerifier(nn.Module):
     def __init__(self):
         super().__init__()
         self.clip, _ = clip.load("ViT-B/32", device=DEVICE)
         self.clip.float()
-        for p in self.clip.parameters():
-            p.requires_grad = True
-
-        for p in self.clip.transformer.parameters():
-            p.requires_grad = False
 
         # Scale the similarity scores. Higher values make output logits more confident.
         self.temperature = nn.Parameter(torch.tensor([1.0], device=DEVICE))
@@ -37,7 +36,7 @@ class SignatureVerifier(nn.Module):
     def forward(self, images):
         images = images.to(device=DEVICE)
 
-        with torch.autocast(device_type=DEVICE.type):
+        with torch.autocast(device_type=DEVICE):
             image_features = self.clip.encode_image(images)
             image_features = image_features / image_features.norm(dim=1, keepdim=True)
 
@@ -45,21 +44,23 @@ class SignatureVerifier(nn.Module):
 
         return logits
     
-def predict_signature_type(image_path, model_path = ""):
-    gdown.download(f"https://drive.google.com/uc?id={MODEL_PATH_ID}", model_path, quiet=False)
+def predict_signature_type(image_path, model_path = MODEL_PATH):
+    # gdown.download(f"https://drive.google.com/uc?id={MODEL_PATH_ID}", model_path, quiet=False)
     model = SignatureVerifier().to(DEVICE)
     model.load_state_dict(torch.load(model_path, map_location=DEVICE))
     model.eval()
 
     orig_img, tensor_image = preprocess(image_path)
 
-    with torch.no_grad(), torch.autocast(device_type=DEVICE.type):
+    with torch.no_grad(), torch.autocast(device_type=DEVICE):
         output = model(tensor_image)
         probs = torch.softmax(output, dim=1)
         pred = torch.argmax(probs, dim=1).item()
 
     label = "Genuine" if pred == 1 else "Forged"
     confidence = probs[0][pred].item()
+
+    print(f"Prediction: {label} ({confidence:.2f})")
 
     plt.imshow(orig_img)
     plt.title(f"Prediction: {label} ({confidence:.2f})")
