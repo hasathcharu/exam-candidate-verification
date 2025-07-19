c271ef1aa67a21aafea23b3c7a56085c45c25f6c
Haritha Hasathcharu <42366642+hasathcharu@users.noreply.github.com>
2025-07-19 10:56:20 +0530

    Make minor changes to the UI and changed model locations

--- frontend/src/components/loading-dialog.tsx
+++ frontend/src/components/loading-dialog.tsx
@@ -1,8 +1,11 @@
 import { AlertDialogTitle } from './ui/alert-dialog';
 import Orb from './orb';
-export default function ({title}:{title:string}) {
+import { AlertDialogDescription } from '@radix-ui/react-alert-dialog';
+export default function ({ title }: { title: string }) {
   return (
     <div className='text-center'>
+      <AlertDialogDescription>
+      </AlertDialogDescription>
       <Orb />
       <AlertDialogTitle>{title}</AlertDialogTitle>
     </div>
