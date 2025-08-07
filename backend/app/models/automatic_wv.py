a202d3a5bfb7fcee75966a7ca0ea1505abbc68cd
RashmiSandamini <rashmi20sandamini@gmail.com>
2025-08-07 11:47:24 +0530

    Minor changes

--- backend/app/models/automatic_wv.py
+++ backend/app/models/automatic_wv.py
@@ -3,62 +3,19 @@ from ..helpers.automatic_wv.texture_creation.texture_creation import create_text
 from werkzeug.exceptions import InternalServerError
 import os
 import matplotlib.pyplot as plt
-import tensorflow as tf
-import cv2
 import numpy as np
-from keras.saving import register_keras_serializable
-from tensorflow.keras import backend as K
 import re
-import joblib
 import torch
 import torch.nn as nn
 from transformers import ViTModel, ViTConfig
 import torch.nn.functional as F
 from torchvision import transforms
 from PIL import Image
-from ..helpers.automatic_wv.utils.constants import SIAMESE_MODEL_PATH, SIAMESE_PATH_ID
+from ..helpers.automatic_wv.utils.constants import SIAMESE_MODEL_PATH, SIAMESE_PATH_ID, IMG_SIZE, TWO_SPEED_MODEL_PATH
 import gdown
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 FILE_PATH = os.path.dirname(__file__)
-IMG_SIZE = (384,384)
-
-@register_keras_serializable()
-def l2_normalize(x):
-    return tf.math.l2_normalize(x, axis=1)
-
-@register_keras_serializable()
-def euclidean_distance(vects):
-    x, y = vects
-    sum_square = tf.reduce_sum(tf.square(x - y), axis=1, keepdims=True)
-    return tf.sqrt(tf.maximum(sum_square, K.epsilon()))
-
-@register_keras_serializable()
-def contrastive_loss(y_true, y_pred):
-    square_pred = tf.square(y_pred)
-    margin = 0.7
-    margin_square = tf.square(tf.maximum(margin - y_pred, 0))
-    return tf.reduce_mean((1 - y_true) * square_pred + y_true * margin_square)
-
-@register_keras_serializable()
-def output_shape(input_shapes):
-    return (input_shapes[0][0], 1)
-
-MODEL_VGG = tf.keras.models.load_model(
-os.path.join(FILE_PATH,"../weights/siamese_model.keras"),
-custom_objects={
-    "l2_normalize": l2_normalize,
-    "euclidean_distance": euclidean_distance,
-    "contrastive_loss": contrastive_loss,
-    "output_shape": output_shape
-},
-safe_mode=False
-)
-FEATURE_EXTRACTOR_VGG = MODEL_VGG.get_layer("cnn_backbone")
-
-CLF_PATH = os.path.abspath(os.path.join(FILE_PATH, "../weights/logistic_model.joblib"))
-clf = joblib.load(CLF_PATH)
 
 vit_name = "google/vit-base-patch16-384"
 config = ViTConfig.from_pretrained(vit_name)
@@ -145,13 +102,11 @@ if not os.path.exists(SIAMESE_MODEL_PATH):
     print(f"Model file not found at {SIAMESE_MODEL_PATH}. Downloading...")
     os.makedirs(os.path.dirname(SIAMESE_MODEL_PATH), exist_ok=True)
     gdown.download(f"https://drive.google.com/uc?id={SIAMESE_PATH_ID}", SIAMESE_MODEL_PATH, quiet=False)
-
-SIAMESE_MODEL.load_state_dict(torch.load(os.path.join(FILE_PATH,"../weights/siamese_network.pt"), map_location=device))
+SIAMESE_MODEL.load_state_dict(torch.load(SIAMESE_MODEL_PATH, map_location=device))
 
 TWO_SPEED_MODEL = PairNet()
-checkpoint = torch.load(os.path.join(FILE_PATH, "../weights/pairnet_model.pth"), weights_only=False)
+checkpoint = torch.load(TWO_SPEED_MODEL_PATH, weights_only=False)
 TWO_SPEED_MODEL.load_state_dict(checkpoint["model_state_dict"])
-
 SPEED_THRESHOLD = checkpoint["best_threshold"]
 
 @torch.no_grad()
@@ -167,17 +122,6 @@ def preprocess_image(image, save_path=None):
     if save_path is not None:
         plt.imsave(save_path, binary_image, cmap="gray")
     return binary_image
-
-def preprocess_image_for_vgg16(img_path):
-    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    if img is None:
-        raise ValueError(f"Failed to read image: {img_path}")
-    img = cv2.resize(img, IMG_SIZE)
-    img = np.expand_dims(img, axis=-1)
-    img = np.repeat(img, 3, axis=-1)
-    img = img.astype(np.float32) / 255.0
-    img = np.expand_dims(img, axis=0)
-    return img
 
 transform = transforms.Compose([
     transforms.Resize(IMG_SIZE),
@@ -202,16 +146,6 @@ def compute_embeddings(patches):
     embeddings = np.stack(embeddings, axis=0)         
     avg_embedding = np.mean(embeddings, axis=0)    
     return avg_embedding
-
-def compute_embeddings_vgg16(patches):
-    embeddings = []
-    for patch in patches:
-        input = preprocess_image_for_vgg16(patch)
-        embedding = FEATURE_EXTRACTOR_VGG.predict(input)
-        embeddings.append(embedding[0])
-    embeddings = np.stack(embeddings, axis=0)  
-    avg_embedding = np.mean(embeddings, axis=0)
-    return avg_embedding 
 
 def preprocess_and_create_textures(input_path, binary_output_path, textures_output_dir, prefix):
     binary_image = preprocess_image(input_path, binary_output_path)
@@ -319,9 +253,6 @@ def perform_pairwise_automatic_writer_verification(file1_normal_path, file1_fast
         prob = torch.sigmoid(output).item()
     
     same_writer = prob <= SPEED_THRESHOLD
-    # label = clf.predict(X_new)[0]
-    # prob = clf.predict_proba(X_new)[0,label]
-    print(same_writer, prob, output.item())
     return {
         "probability": float(prob),
         "same_writer": bool(same_writer)
